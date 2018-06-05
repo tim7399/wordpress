a2e388998f8baf7587092677ca3a6522a5b108cf
tim7399 <39880908+tim7399@users.noreply.github.com>
2018-06-04 20:17:43 -0400

    disallowing plugin installs via web interface

--- wp-config.php
+++ wp-config.php
@@ -76,6 +76,8 @@ define('WP_DEBUG', false);
 define('WP_SITEURL', 'http://'.$_SERVER['HTTP_HOST'].'/');
 define('WP_HOME', 'http://'.$_SERVER['HTTP_HOST'].'/');
 
+define('DISALLOW_FILE_MODS',true);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
